5a38f48d1b3cc6d8b07fda716d43e5adf38e19b7
Evan Burch <edburch42@tntech.edu>
2022-10-31 03:14:52 -0500

    Fixed test_get_all_artists to support chaning indexes in mongo

--- test/test_mongobridge.py
+++ test/test_mongobridge.py
@@ -10,8 +10,12 @@ class TestMongoBridge(TestCase):
     def test_get_all_artists(self):
         artists = self.mongo_bridge.get_all_artists()
         self.assertEqual(179, len(artists))
-        self.assertEqual(1826136, artists[60]["artistID"])
-        self.assertEqual(628155, artists[178]["artistID"])
+    #    self.assertEqual(1826136, artists[60]["artistID"])
+    #    self.assertEqual(628155, artists[178]["artistID"])
+        a0 = next(item for item in artists if item["artistID"] == 1826136)["artistName"]
+        self.assertEqual("Stephen Tarter", a0)
+        a1 = next(item for item in artists if item["artistID"] == 628155)["artistName"]
+        self.assertEqual("A. P. Carter", a1)
 
     def test_get_artists_from_list(self):
         ids = [938895, 2634203, 1141486, 908705, 2411933, 2304638, 3895080, 1448909, 1448911, 1141474, 2916175, 353265, 1141476, 938862, 1141491, 1141484, 1141487, 307357, 1141480, 516930, 1001138, 1141475, 269365, 1141488, 1141483, 1141489, 2867358, 2867360, 2189637, 908699, 1420640, 2867359, 1826135]
