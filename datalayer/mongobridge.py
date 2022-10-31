69a6bc191f7bcb35d9873d9b175eaebcd862c68e
Evan Burch <edburch42@tntech.edu>
2022-10-31 01:36:34 -0500

    Added code to mongobridge.py to pass given tests

--- datalayer/mongobridge.py
+++ datalayer/mongobridge.py
@@ -12,14 +12,16 @@ class MongoBridge(object):
         """
         Connects to the uri server, db database, and the col collection
         """
-        pass
+        self.__mongoClient: pymongo.MongoClient = pymongo.MongoClient(uri)
+        self.__myCollection: pymongo.collection = self.__mongoClient[db][col]
 
     @dispatch()
     def __init__(self):
         """
         Connects to the mongo server, BristolData database, and the Artists collection
         """
-        pass
+        self.__mongoClient: pymongo.MongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.__myCollection: pymongo.collection = self.__mongoClient["BristolData"]["Artists"]
 
     def get_all_artists(self) -> List[dict]:
         """
@@ -35,7 +37,11 @@ class MongoBridge(object):
         See the test_artist.py for an example
         :return: list of the dictionaries returned from mongo
         """
-        pass
+        result: List[dict] = []
+        artists = self.__myCollection.find()
+        for a in artists:
+            result.append(a)
+        return result
 
     def get_artists_from_list(self, a_list: list[int]) -> List[dict]:
         """
@@ -51,7 +57,11 @@ class MongoBridge(object):
         See the test_artist.py for an example
         :return: list of the dictionaries returned from mongo
         """
-        pass
+        result: List[dict] = []
+        for i in a_list:
+            afilter = {"artistID": i}
+            result.append(self.__myCollection.find_one(afilter))
+        return result
 
     def get_artist_by_id(self, aid: int) -> dict:
         """
@@ -68,4 +78,6 @@ class MongoBridge(object):
         :param aid: artist id
         :return: dictionary with artist info
         """
-        pass
+        afilter = {"artistID": aid}
+        a = self.__myCollection.find_one(afilter)
+        return a
